855ad686ac297ebbeec2ea03b7119fc3002ad36a
Abhishek <chaudharyabhishekmp@gmail.com>
2024-08-10 13:12:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import FeatureSection from "./components/FeatureSection";
+import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import Testimonials from "./components/Testimonials";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Testimonials />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
